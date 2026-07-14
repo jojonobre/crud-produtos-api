acfa08df601813d9856ef105cbab3ea71e090446
Joyce Maria <jojonobre1810@gmail.com>
2026-07-14 15:03:41 -0300

    Add files via upload

---
+++ crud-produtos-api/src/main/java/com/joyce/crud_produtos_api/CrudProdutosApiApplication.java
@@ -0,0 +1,13 @@
+package com.joyce.crud_produtos_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudProdutosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudProdutosApiApplication.class, args);
+	}
+
+}
---
+++ crud-produtos-api/src/test/java/com/joyce/crud_produtos_api/CrudProdutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joyce.crud_produtos_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudProdutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
